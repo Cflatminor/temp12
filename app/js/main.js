bb97f75e05b651baad1ef1a73259d07160d476a0
Roman Bogdanov <cflatminor92@gmail.com>
2019-02-06 19:04:21 +0200

    watcher sync ready

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,9 @@ console.log ('first taste');
 console.log ('second taste');
 
 console.log ('third taste');
+
+function summ (a, b) {
+  return a + b;
+}
+
+console.log (summ (2,2));
--- app/js/test2.js
+++ app/js/test2.js
@@ -1,3 +1,9 @@
 console.log ('second taste');
 
 console.log ('third taste');
+
+function summ (a, b) {
+  return a + b;
+}
+
+console.log (summ (2,2));
