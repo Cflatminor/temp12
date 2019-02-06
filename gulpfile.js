713a904fad5cf877e6bb802a470772832d94b376
Roman Bogdanov <cflatminor92@gmail.com>
2019-02-06 18:17:34 +0200

    managin project

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 let gulp = require ('gulp'),
-		sass = require ('gulp-sass');
+		sass = require ('gulp-sass'),
+		concat = require('gulp-concat'),
+		browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
 
 function def () {
 	return console.log ('Ready!');
 }
 
+function sassCompile () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+		.pipe(concat('main.css'))
+    .pipe(gulp.dest('app/css'));
+};
+
+function sync () {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+};
+
 gulp.task ('default', def);
+gulp.task ('sass-compile', sassCompile);
+gulp.task ('browser-sync', sync);
+gulp.task ('watch', function () {
+	gulp.watch('app/scss/**/*.scss', gulp.series('sass-compile'));
+});
