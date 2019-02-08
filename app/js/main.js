70bf4ce00ed62c82ad9a87d21b2cb61b8a2a8dfb
Roman Bogdanov <cflatminor92@gmail.com>
2019-02-08 18:04:54 +0200

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,27 @@
-// Библиотеки
-// import 'jquery';
-import '../libs/vue/vue.js';
+const mainHeader = document.getElementById('header');
+let name = 'vvvv';
 
-// Основной код
-import './test';
-import './test2';
+function createUser (username) {
+  console.log (name);
+  console.log (username);
+  return name = username;
+};
+
+mainHeader.innerHTML = '<p>hhh!</p> <p>message</p> <p>UserName is : ' + name + '</p>';
+
+let cat = {
+  name: 'Alice',
+  age: 4,
+  sayMeow: function () {
+    return console.log ('meow meow');
+  },
+  eat: function () {
+    return console.log ('nyam nyam');
+  }
+}
+
+cat.sayMeow();
+cat.eat();
+createUser('aaaa');
+
+console.log (name)
